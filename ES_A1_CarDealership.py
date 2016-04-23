efb346b85b1cbb8d60ce7cad6c4832574478e74a
PedroStamm <pedro_7915@hotmail.com>
2016-04-23 01:07:18 +0100

    I don't even know, tons of stuff, losing my mind

--- ES_A1_CarDealership.py
+++ ES_A1_CarDealership.py
@@ -1,14 +1,15 @@
 # Flask Server developed for Assignment 1 of the 2016
 # Services Engineering course from the Masters in Informatics Engineering
 # by Pedro Stamm
-import flask
-from sqlalchemy.ext.declarative import DeclarativeMeta
+import time
 from flask import *
 from sqlalchemy import *
+from sqlalchemy.ext.declarative import DeclarativeMeta
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import *
 
 app = Flask(__name__)
+app.secret_key = 'l\x07=J#\x160\xc9\xf46\x8c\xcc\xea\x85\xb9\x1d3\x93~>a\x9c\xc6:'
 
 engine = create_engine('postgresql+pg8000://FlaskServer:ES2016@localhost:5432/ESDealership')
 Base = declarative_base()
@@ -99,117 +100,270 @@ Session = sessionmaker(bind=engine)
 
 # Example code
 """
-session = Session()
+ses = Session()
 
 dio_client = Client(email='dio@adventure.jp', password='TheWorld',
                     name='Dio Brando', district='London',
                     description='Za Warudo')
 
-session.add(dio_client)
+ses.add(dio_client)
 try:
-    session.commit()
+    ses.commit()
 except Exception as e:
-    session.rollback()
+    ses.rollback()
     print("Issue adding user to DB")
     print(e)
 
-our_client = session.query(Client).filter_by(name='Dio Brando').first()
+our_client = ses.query(Client).filter_by(name='Dio Brando').first()
 print(our_client)
+
+try:
+    ses.delete(our_client)
+    ses.commit()
+    print("DIO WAS DELETED")
+except Exception as e:
+    ses.rollback()
+    print("DIO LIVES")
+    print(e)
 """
 
 
+def validate_login(email, token, type):
+    ses = Session()
+    if type == 'client':
+        r = ses.query(Client).filter_by(email=email).first()
+        if r and r.email == email:
+            return True
+    if type == 'owner':
+        r = ses.query(Owner).filter_by(email=email).first()
+        if r and r.email == email:
+            return True
+    return False
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    if 'email' in session:
+        if validate_login(session['email'], session['token'], session['type']):
+            return "Logged in as %s" % escape(session['email'])
+    return render_template('login.html')
+
+
+@app.route('/owner/login')
+def owner_login():
+    return render_template('login_owner.html')
+
+
+@app.route('/client/login')
+def client_login():
+    return render_template('login_client.html')
 
 
 @app.route('/register/')
-def register():
+@app.route('/register/<type>')
+def register(type=None):
+    if type == 'client':
+        return render_template('register_client.html')
+    elif type == 'owner':
+        return render_template('register_owner.html')
     return render_template('register.html')
 
 
-@app.route('/register/owner')
-def register_owner():
-    return render_template('register_owner.html')
+# Everything API from here downwards
 
 
-@app.route('/register/client')
-def register_client():
-    return render_template('register_client.html')
-
-
-@app.route('/api/user/client', methods=['GET', 'PUT'])
-def handle_client():
-    if request.method == 'PUT':
-        print("Received PUT request")
-        data = request.form.to_dict()
-        email = data['email']
-        password = data['password']
-        name = data['name']
-        district = data['district']
-        description = data['description']
-        newClient = Client(email=email, password=password, name=name, district=district,
-                           description=description)
-        session = Session()
-        try:
-            session.add(newClient)
-            session.commit()
-            print("Client " + email + " added to database")
-            return jsonify(
-                result="Client added",
-                bool=True
-            )
-        except Exception as e:
-            session.rollback()
-            print("Issue adding Client to DB")
-            print(e)
-            return jsonify(
-                result="Failed to add Client",
-                bool=False
-            )
-    if request.method == 'GET':
-        session = Session()
-        result = session.query(Client)
-        arr = []
-        for r in result:
-            arr.append(json.dumps(r, cls=AlchemyEncoder))
-        return Response(json.dumps(arr), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
-
-
-@app.route('/api/user/owner', methods=['GET', 'PUT'])
-def handle_owner():
-    if request.method == 'PUT':
-        print("Received PUT request")
+@app.route('/api/owner/', methods=['GET', 'POST', 'PUT'])
+@app.route('/api/owner/<email>', methods=['GET'])
+def handle_owner(email=None):
+    if request.method == 'POST':
+        print("Received POST request")
         data = request.form.to_dict()
         email = data['email']
         password = data['password']
         name = data['name']
         description = data['description']
         newOwner = Owner(email=email, password=password, name=name, description=description)
-        session = Session()
+        ses = Session()
         try:
-            session.add(newOwner)
-            session.commit()
+            ses.add(newOwner)
+            ses.commit()
             print("Owner " + name + " added to database")
             return jsonify(
                 result="Owner added",
                 bool=True
             )
         except Exception as e:
-            session.rollback()
+            ses.rollback()
             print("Issue adding Owner to DB")
             print(e)
             return jsonify(
                 result="Failed to add Owner",
                 bool=False
             )
+    if request.method == 'PUT':
+        if ('email' in session) and session['type'] == 'owner' and validate_login(session['email'], session['token'], session['type']):
+            data = request.form.to_dict()
+            password = data['password']
+            name = data['name']
+            description = data['description']
+            ses = Session()
+            res = ses.query(Owner).filter_by(email=session['email']).first()
+            res.name = name
+            if password:
+                res.password = password
+            res.description = description
+            try:
+                ses.commit()
+                print("Owner " + name + " updated")
+                return jsonify(
+                    result="Owner updated",
+                    bool=True
+                )
+            except Exception as e:
+                ses.rollback()
+                print("Issue updating Owner")
+                print(e)
+                return jsonify(
+                    result="Failed to update Owner",
+                    bool=False
+                )
+        return jsonify(result="Unauthorized access");
     if request.method == 'GET':
-        session = Session()
-        result = session.query(Owner)
-        arr = []
-        for r in result:
-            arr.append(json.dumps(r, cls=AlchemyEncoder))
-        return Response(json.dumps(arr), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+        if 'email' in session and validate_login(session['email'], session['token'], session['type']):
+            ses = Session()
+            if email is None:
+                result = ses.query(Owner)
+                arr = []
+                for r in result:
+                    arr.append(json.dumps(r, cls=AlchemyEncoder))
+                return Response(json.dumps(arr), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+            else:
+                result = ses.query(Owner).filter_by(email=email)
+                return json.dumps(result.first(), cls=AlchemyEncoder)
+        return jsonify(result="Unauthorized access")
+
+
+@app.route('/api/owner/login', methods={'POST'})
+def login_owner():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        email = data['email']
+        password = data['password']
+        ses = Session()
+        res = ses.query(Owner).filter_by(email=email).first()
+        if res and password == res.password:
+            session['email'] = email
+            session['token'] = int(time.time() * 1000)
+            session['type'] = 'owner'
+            return jsonify(login=True)
+        return jsonify(login=False)
+
+
+@app.route('/api/owner/logout')
+def logout_owner():
+    if 'email' in session:
+        session.pop('email', None)
+        return redirect(url_for('index'))
+
+
+@app.route('/api/client/', methods=['GET', 'POST', 'PUT'])
+@app.route('/api/client/<email>', methods=['GET'])
+def handle_client(email=None):
+    if request.method == 'POST':
+        print("Received POST request")
+        data = request.form.to_dict()
+        email = data['email']
+        password = data['password']
+        name = data['name']
+        district = data['district']
+        description = data['description']
+        newClient = Client(email=email, password=password, name=name, district=district, description=description)
+        ses = Session()
+        try:
+            ses.add(newClient)
+            ses.commit()
+            print("Client " + name + " added to database")
+            return jsonify(
+                result="Client added",
+                bool=True
+            )
+        except Exception as e:
+            ses.rollback()
+            print("Issue adding Client to DB")
+            print(e)
+            return jsonify(
+                result="Failed to add Client",
+                bool=False
+            )
+    if request.method == 'PUT':
+        if ('email' in session) and session['type'] == 'client' and validate_login(session['email'], session['token'],
+                                                                                  session['type']):
+            data = request.form.to_dict()
+            password = data['password']
+            name = data['name']
+            district = data['district']
+            description = data['description']
+            ses = Session()
+            res = ses.query(Client).filter_by(email=session['email']).first()
+            res.name = name
+            if password:
+                res.password = password
+            res.district = district
+            res.description = description
+            try:
+                ses.commit()
+                print("Owner " + name + " updated")
+                return jsonify(
+                    result="Owner updated",
+                    bool=True
+                )
+            except Exception as e:
+                ses.rollback()
+                print("Issue updating Owner")
+                print(e)
+                return jsonify(
+                    result="Failed to update Owner",
+                    bool=False
+                )
+        return jsonify(result="Unauthorized access");
+    if request.method == 'GET':
+        if 'email' in session and validate_login(session['email'], session['token'], session['type']):
+            ses = Session()
+            if email is None:
+                result = ses.query(Client)
+                arr = []
+                for r in result:
+                    arr.append(json.dumps(r, cls=AlchemyEncoder))
+                return Response(json.dumps(arr), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+            else:
+                result = ses.query(Client).filter_by(email=email)
+                return json.dumps(result.first(), cls=AlchemyEncoder)
+        return jsonify(result="Unauthorized access");
+
+
+@app.route('/api/client/login', methods={'POST'})
+def login_client():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        email = data['email']
+        password = data['password']
+        ses = Session()
+        res = ses.query(Client).filter_by(email=email).first()
+        if res and password == res.password:
+            session['email'] = email
+            session['token'] = int(time.time() * 1000)
+            session['type'] = 'client'
+            return jsonify(login=True)
+        return jsonify(login=False)
+
+
+@app.route('/api/client/logout')
+def logout_client():
+    if 'email' in session:
+        session.pop('email', None)
+        session.pop('token', None)
+        session.pop('type', None)
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
